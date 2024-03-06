b3d060c139b1fc4c70d37f7762862c2b9d9a5726
mozta <perezaguirrerafael@gmail.com>
2024-03-06 17:29:04 -0600

    data todo list

--- app.py
+++ app.py
@@ -1,25 +1,81 @@
 from flask import Flask, jsonify, request, abort
+from datetime import datetime
 
 app = Flask(__name__)
 
-tasks = []
+tasks = [
+    {
+        "category": "Comida",
+        "created": "Mon, 04 Mar 2024 18:02:12 GMT",
+        "id": 1,
+        "name": "Comer",
+        "status": False,
+        "updated": "Mon, 04 Mar 2024 18:02:12 GMT"
+    },
+    {
+        "category": "Escuela",
+        "created": "Mon, 04 Mar 2024 18:02:14 GMT",
+        "id": 2,
+        "name": "Dar la clase",
+        "status": False,
+        "updated": "Mon, 04 Mar 2024 18:02:14 GMT"
+    }
+]
+
 BASE_URL = '/api/v1/'
 
 
 @app.route('/')
 def home():
-    return 'Welcome to my To-Do List'
+    return abort(400, erro r='Missing body in request')
 
 
-@app.route('/api/v1/tasks', methods=['POST'])
+@app.route(BASE_URL + 'tasks', methods=['POST'])
 def create_task():
-    print("entreee")
     if not request.json:
-        print("aaaaaaa")
-        return 'aaaaaa'#abort(404)
-    print("eeeeee")
-    return 'eeeee'
+        abort(400, error='Missing body in request')
+    this_time = datetime.now()
+    task = {
+        'id': len(tasks) + 1,
+        'name': request.json['name'],
+        'category': request.json['category'],
+        'status': False,
+        'created': this_time,
+        'updated': this_time,
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
+
+@app.route(BASE_URL + 'tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({"tasks": tasks})
+
+
+@app.route(BASE_URL + 'tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    this_task = [task for task in tasks if task['id'] == id]
+    print("TASK")
+    if len(this_task) == 0:
+        abort(404, error='ID not found!')
+    return jsonify({'task': this_task[0]})
+
+
+@app.route(BASE_URL + 'tasks/<int:id>', methods=['PUT'])
+def check_task(id):
+    this_task = [task for task in tasks if task['id'] == id]
+    if len(this_task) == 0:
+        abort(404, error='ID not found!')
+    this_task[0]['status'] = not this_task[0]['status']
+    return jsonify({'task': this_task[0]})
+
+@app.route(BASE_URL + 'tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    this_task = [task for task in tasks if task['id'] == id]
+    if len(this_task) == 0:
+        abort(404, error='ID not found!')
+    tasks.remove(this_task[0])
+    return jsonify({'result': True})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
